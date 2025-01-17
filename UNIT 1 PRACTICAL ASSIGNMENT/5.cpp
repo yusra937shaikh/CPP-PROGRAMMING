008ad677d2af14424170810d8dbcf7e1c568219d
YUSRA SHAIKH <yusra937shaikh@gmail.com>
2025-01-17 18:56:52 +0530

    Create 5.cpp

---
+++ UNIT 1 PRACTICAL ASSIGNMENT/5.cpp
@@ -0,0 +1,26 @@
+//write a c++ program to find simple isterest. principal amount ,time period, and rate must be taken from user.
+#include<iostream>
+using namespace std;
+class si
+{
+   public:
+   	int year;
+   	float pa,rate,si;
+   	void cal()
+   	{
+   		cout<<endl<<"ENTER THE PRINCIPAL AMOUNT:";
+   		cin>>pa;
+   		cout<<endl<<"ENTER THE RATE:";
+   		cin>>rate;
+   		cout<<endl<<"ENTER THE YEAR:";
+   		cin>>year;
+   		si=(pa*rate*year)/100;
+   		cout<<endl<<"SIMPLE INTEREST IS:"<<si;
+   	}
+
+};
+int main()
+{
+	si a;
+	a.cal();
+}
